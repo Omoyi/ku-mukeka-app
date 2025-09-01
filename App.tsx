e52ed7a19a4cc79f02b25420db8550b205e50f3f
Rachel <urakhel7@gmail.com>
2025-09-01 14:39:25 +0200

    Update main route

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
 import HomeScreen from './app/HomeScreen';
 import AuthScreen from './app/AuthScreen';
 import { NavigationContainer } from '@react-navigation/native';
@@ -9,7 +10,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Auth">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Auth" component={AuthScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
