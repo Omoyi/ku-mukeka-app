2271733cc9c0ef9058663f2d373fa9e2fbbee18a
Rachel <urakhel7@gmail.com>
2025-09-01 14:37:57 +0200

    Add metro config file , debug tailwind issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
+  prtsesets: [require("nativewind/preset")],
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
